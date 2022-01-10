af08c47c3f54cc7b82c68bfc1d1e4ad8b7338735
Sidcley Nascimento <sid.nas@outlook.com>
2022-01-10 08:54:40 -0300

    Adição de Barra de Ferramentas e Configuração das Lazy Loading.

--- src/app/cdd/cdd-routing.module.ts
+++ src/app/cdd/cdd-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClassesComponent } from './classes/classes.component';
+
+const routes: Routes = [
+  {path: "", component: ClassesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
